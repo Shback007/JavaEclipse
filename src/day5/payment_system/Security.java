dfc63bc3747355b0ec249fdf46cecf91db1cd9e0
Sagnik Hore <sagnikhoredht@gmail.com>
2026-04-27 17:25:02 +0530

    day5

---
+++ src/day5/payment_system/Security.java
@@ -0,0 +1,24 @@
+package day5.payment_system;
+
+import java.util.Scanner;
+
+public final class Security {
+
+    public String encrypt(String data) {
+		return data;
+    }
+
+    public void decrypt(String data) {
+        System.out.println("Decrypting data: " + data);
+    }
+    public static void main(String[] args) {
+    	Security s1 = new Security();
+    	Scanner sc = new Scanner(System.in);
+    	System.out.print("Enter data for encryption:");
+    	String ss = sc.next();
+    	System.out.println("Encrypting:"+ ss);
+		String abc=s1.encrypt(ss);
+		s1.decrypt(abc);
+		sc.close();
+	}
+}
